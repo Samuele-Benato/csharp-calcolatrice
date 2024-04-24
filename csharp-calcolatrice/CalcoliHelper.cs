33952043761e2cbd0235ef4d32ee6edc7d12641d
Samuele-Benato <samuele.benato@gmail.com>
2024-04-24 15:52:54 +0200

    fix : applied DRY after feedback

--- csharp-calcolatrice/CalcoliHelper.cs
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -9,29 +9,75 @@ namespace csharp_calcolatrice
     static class CalcoliHelper
     {
        
-        public static int SumInt(int a, int b) => a + b;
+        //public static int SumInt(int a, int b) => a + b;
         
-        public static double SumDouble(double a, double b) => (double)a + (double)b;
+        //public static double SumDouble(double a, double b) => (double)a + (double)b;
+        public static T Sum<T>(T a, T b)
+        {
+            dynamic A = a;
+            dynamic B = b;
+
+            return A + B;
+        }
+
+        //public static int DifferenceInt(int a , int b) => a < b ? (b - a) : (a - b);
        
-        public static int DifferenceInt(int a , int b) => a < b ? (b - a) : (a - b);
-       
-        public static double DifferenceDouble(double a, double b) => a < b ? (b - a) : (a - b);  
+        //public static double DifferenceDouble(double a, double b) => a < b ? (b - a) : (a - b);
 
-        public static int MoltiplicationInt(int a, int b) => a * b;
+        public static T Difference<T>(T a, T b) 
+        {
+            dynamic A = a;
+            dynamic B = b;
 
-        public static double MoltiplicationDouble(double a, double b) => a * b;
+            return A < B ? (B - A) : (A - B);
+        }
 
-        public static int AbsoluteValueInt(int number) => number < 0 ? -number : number;
+        //public static int MoltiplicationInt(int a, int b) => a * b;
 
-        public static double AbsoluteValueDouble(double number) => number < 0 ? -number : number;
+        //public static double MoltiplicationDouble(double a, double b) => a * b;
 
-        public static int MinInt(int a, int b) => a < b ? a : b;
+        public static T Moltiplication<T>(T a, T b) 
+        {
+            dynamic A = a;
+            dynamic B = b;
 
-        public static double MinDouble(double a, double b) => a < b ? a : b;
+            return A * B;
+        }
 
-        public static int MaxInt(int a, int b) => a > b ? a : b;
+        //public static int AbsoluteValueInt(int number) => number < 0 ? -number : number;
 
-        public static double MaxDouble(double a, double b) => a > b ? a : b;
+        //public static double AbsoluteValueDouble(double number) => number < 0 ? -number : number;
+
+        public static T AbsoluteValue<T>(T number) 
+        {
+            dynamic Number = number;
+
+            return Number < 0 ? -Number : Number;
+        }
+
+        //public static int MinInt(int a, int b) => a < b ? a : b;
+
+        //public static double MinDouble(double a, double b) => a < b ? a : b;
+
+        public static T Min<T>(T a, T b) 
+        {
+            dynamic A = a;
+            dynamic B = b;
+
+            return A < B ? A : B;
+        }
+
+        //public static int MaxInt(int a, int b) => a > b ? a : b;
+
+        //public static double MaxDouble(double a, double b) => a > b ? a : b;
+
+        public static T Max<T>(T a, T b) 
+        {
+            dynamic A = a;
+            dynamic B = b;
+
+            return A > B ? A : B;
+        }
 
         // BONUS
         public static double Power(double a, double b)
--- csharp-calcolatrice/Program.cs
+++ csharp-calcolatrice/Program.cs
@@ -5,55 +5,81 @@
         static void Main(string[] args)
         {
 
-            // Test del metodo SumInt
-            int sumIntResult = CalcoliHelper.SumInt(2, 3);
-            Console.WriteLine($"Somma intera: {sumIntResult}");
+            //// Test del metodo SumInt
+            //int sumIntResult = CalcoliHelper.SumInt(2, 3);
+            //Console.WriteLine($"Somma intera: {sumIntResult}");
 
-            // Test del metodo SumDouble
-            double sumDoubleResult = CalcoliHelper.SumDouble(2.5, 3.7);
-            Console.WriteLine($"Somma decimali: {sumDoubleResult}");
+            //// Test del metodo SumDouble
+            //double sumDoubleResult = CalcoliHelper.SumDouble(2.5, 3.7);
+            //Console.WriteLine($"Somma decimali: {sumDoubleResult}");
 
-            // Test del metodo DifferenceInt
-            int differenceIntResult = CalcoliHelper.DifferenceInt(4, 13);
-            Console.WriteLine($"Differenza intera: {differenceIntResult}");
 
-            // Test del metodo DifferenceDouble
-            double differenceDoubleResult = CalcoliHelper.DifferenceDouble(5.5, 3.2);
-            Console.WriteLine($"Differenza decimali: {differenceDoubleResult}");
+            // Test del metodo Sum
+            int sumResult = CalcoliHelper.Sum(2, 3);
+            Console.WriteLine($"Somma : {sumResult}");
 
-            // Test del metodo MoltiplicationInt
-            int moltiplicationIntResult = CalcoliHelper.MoltiplicationInt(5, 3);
-            Console.WriteLine($"Moltiplicazione intera: {moltiplicationIntResult}");
+            //// Test del metodo DifferenceInt
+            //int differenceIntResult = CalcoliHelper.DifferenceInt(4, 13);
+            //Console.WriteLine($"Differenza intera: {differenceIntResult}");
 
-            // Test del metodo MoltiplicationInt
-            double moltiplicationDoubleResult = CalcoliHelper.MoltiplicationDouble(5.8, 7.4);
-            Console.WriteLine($"Moltiplicazione decimali: {moltiplicationDoubleResult}");
+            //// Test del metodo DifferenceDouble
+            //double differenceDoubleResult = CalcoliHelper.DifferenceDouble(5.5, 3.2);
+            //Console.WriteLine($"Differenza decimali: {differenceDoubleResult}");
 
-            // Test del metodo AbsoluteValueInt
+            // Test del metodo Difference
+            int differenceResult = CalcoliHelper.Difference(4, 13);
+            Console.WriteLine($"Differenza : {differenceResult}");
+
+            //// Test del metodo MoltiplicationInt
+            //int moltiplicationIntResult = CalcoliHelper.MoltiplicationInt(5, 3);
+            //Console.WriteLine($"Moltiplicazione intera: {moltiplicationIntResult}");
+
+            //// Test del metodo MoltiplicationInt
+            //double moltiplicationDoubleResult = CalcoliHelper.MoltiplicationDouble(5.8, 7.4);
+            //Console.WriteLine($"Moltiplicazione decimali: {moltiplicationDoubleResult}");
+
+            // Test del metodo Moltiplication
+            int moltiplicationResult = CalcoliHelper.Moltiplication(5, 3);
+            Console.WriteLine($"Moltiplicazione : {moltiplicationResult}");
+
+            //// Test del metodo AbsoluteValueInt
+            //int number = -5;
+            //int absValue = CalcoliHelper.AbsoluteValueInt(number);
+            //Console.WriteLine($"Il valore assoluto di {number} è {absValue}");
+
+            //// Test del metodo AbsoluteValueDouble
+            //double number2 = -5.5;
+            //double absValue2 = CalcoliHelper.AbsoluteValueDouble(number2);
+            //Console.WriteLine($"Il valore assoluto di {number2} è {absValue2}");
+
+            // Test del metodo AbsoluteValue
             int number = -5;
-            int absValue = CalcoliHelper.AbsoluteValueInt(number);
+            int absValue = CalcoliHelper.AbsoluteValue(number);
             Console.WriteLine($"Il valore assoluto di {number} è {absValue}");
 
-            // Test del metodo AbsoluteValueDouble
-            double number2 = -5.5;
-            double absValue2 = CalcoliHelper.AbsoluteValueDouble(number2);
-            Console.WriteLine($"Il valore assoluto di {number2} è {absValue2}");
+            //// Test del metodo MinInt
+            //int numberminInt = CalcoliHelper.MinInt(5, 3);
+            //Console.WriteLine($"Numero più piccolo: {numberminInt}");
 
-            // Test del metodo MinInt
-            int numberminInt = CalcoliHelper.MinInt(5, 3);
-            Console.WriteLine($"Numero più piccolo: {numberminInt}");
+            //// Test del metodo MinDouble
+            //double numberminDouble = CalcoliHelper.MinDouble(5.7, 3.8);
+            //Console.WriteLine($"Numero più piccolo: {numberminDouble}");
 
-            // Test del metodo MinDouble
-            double numberminDouble = CalcoliHelper.MinDouble(5.7, 3.8);
-            Console.WriteLine($"Numero più piccolo: {numberminDouble}");
+            // Test del metodo Min
+            int numbermin = CalcoliHelper.Min(5, 3);
+            Console.WriteLine($"Numero più piccolo: {numbermin}");
 
-            // Test del metodo MaxInt
-            int numbermaxInt = CalcoliHelper.MaxInt(5, 3);
-            Console.WriteLine($"Numero più grande: {numbermaxInt}");
+            //// Test del metodo MaxInt
+            //int numbermaxInt = CalcoliHelper.MaxInt(5, 3);
+            //Console.WriteLine($"Numero più grande: {numbermaxInt}");
 
-            // Test del metodo MinDouble
-            double numbermaxDouble = CalcoliHelper.MaxDouble(5.7, 3.8);
-            Console.WriteLine($"Numero più grande: {numbermaxDouble}");
+            //// Test del metodo MinDouble
+            //double numbermaxDouble = CalcoliHelper.MaxDouble(5.7, 3.8);
+            //Console.WriteLine($"Numero più grande: {numbermaxDouble}");
+
+            // Test del metodo Max
+            int numbermax = CalcoliHelper.Max(5, 3);
+            Console.WriteLine($"Numero più grande: {numbermax}");
 
             // DRY - Don't Repeat Yourself
 
